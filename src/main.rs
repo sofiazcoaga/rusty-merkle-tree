1f436fad038bbb736d8137600b0f88fadd03d75c
Sofi Azcoaga <sofiaazc3@hotmail.com>
2025-06-05 09:27:18 -0300

    add some spaces between lines as suggested in https://github.com/sofiazcoaga/rusty-merkle-tree/pull/1#discussion_r2127190391

--- src/main.rs
+++ src/main.rs
@@ -9,34 +9,41 @@ pub fn create_merkle_tree_from_data(inputs: &Input) -> MerkleTree {
     let mut merkle_tree: MerkleTree = Vec::new();
     let data_hashes_level = create_data_hashes(inputs);
     merkle_tree.push(data_hashes_level.clone());
+
     let mut previous_level = data_hashes_level;
     let mut level_len = previous_level.len();
+
     while level_len != 1 {
         let new_level = create_new_level(&previous_level);
         merkle_tree.insert(0, new_level.clone());
         previous_level = new_level;
         level_len = previous_level.len();
     }
+
     merkle_tree
 }
 
 fn create_data_hashes(inputs: &Input) -> Vec<String> {
     let mut data_hashes = Vec::new();
+
     for i in inputs {
         data_hashes.push(sha256::digest(i));
     }
+
     data_hashes
 }
 
 fn create_new_level(previous_level: &[String]) -> Vec<String> {
     let mut index = 0;
     let mut new_level = Vec::new();
+
     while index < previous_level.len() {
         let element_concat = previous_level[index].clone() + &previous_level[index + 1];
         let element_hash = sha256::digest(element_concat);
         new_level.push(element_hash);
         index += 2;
     }
+
     new_level
 }
 
@@ -52,6 +59,7 @@ mod test {
         let data_4 = Vec::from(b"lola");
         let mock_data: Input = vec![data_1, data_2, data_3, data_4];
         let hashes = create_data_hashes(&mock_data);
+
         for i in 0..hashes.len() {
             assert_eq!(hashes[i], sha256::digest(mock_data[i].clone()));
         }
@@ -72,6 +80,7 @@ mod test {
 
         let merkle_tree = create_merkle_tree_from_data(&mock_data);
         assert_eq!(merkle_tree.len(), 3);
+
         for i in 0..merkle_tree.len() {
             if i != merkle_tree.len() - 1 {
                 let higher_level = merkle_tree[i].clone();

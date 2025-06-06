18017b62976584e3cf6261178ee0bf8de2e03e84
Sofi Azcoaga <sofiaazc3@hotmail.com>
2025-06-06 10:51:16 -0300

    format with cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use std::vec;
 use lazy_static::lazy_static;
+use std::vec;
 
 // The hash of the zero value
-lazy_static! { static ref DEFAULT_ZERO_HASH: String = sha256::digest(&[0]); }
+lazy_static! {
+    static ref DEFAULT_ZERO_HASH: String = sha256::digest(&[0]);
+}
 
 fn main() {
     println!("Hello, world!");
